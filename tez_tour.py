35f85ca0d8d5bbd0fdf3ccbb318a31a303c5ee60
Shady0492 <arutiuneant4@gmail.com>
2025-05-27 13:58:57 +0300

    Update tez_tour.py

--- tez_tour.py
+++ tez_tour.py
@@ -128,9 +128,9 @@ async def on_startup(app):
 def main():
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     PORT = int(os.getenv("PORT", "8443"))
-    WEBHOOK_URL = f"https://tez-tour-telegram-bot.onrender.com/{BOT_TOKEN}"
+    WEBHOOK_URL = f"https://tez-tour-telegram-bot.onrender.com/{7814465556:AAHPZfY0nh56gOrs-fLjxJ8XUAiFnwgNsmg}"
 
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app = ApplicationBuilder().token(7814465556:AAHPZfY0nh56gOrs-fLjxJ8XUAiFnwgNsmg).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
